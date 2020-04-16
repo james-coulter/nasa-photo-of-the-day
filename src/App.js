cca71d29ea81a3205739cda452d071286e2a7e44
Christian Coulter <christiancoulter@Christians-MacBook-Air-2.local>
2020-04-16 15:52:42 -0600

    Finished styling page using TailwindCSS

--- src/App.js
+++ src/App.js
@@ -55,20 +55,30 @@ function App() {
 `;
 
   return (
-    <div className="App">
+
+    <div className="App" className="m-0 flex flex-col w-screen justify-center bg-gray-800 text-gray-100">
+                      <div className="max-h-screen">
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'></link>
+    <div id='stars'></div>
+    <div id='stars2'></div>
+    <div id='stars3'></div>
+    </div>
       <Alert color="primary">
   Welcome! This site automatically generates today's photo of the day provided by the NASA API: APOD. Feel free to change to previous dates to explore previous images.
 </Alert>
-      <div className="headerDiv">
-      <h1>astronomy photo of the day</h1>
-      <h4 className="smallHeader">created via nasa's open api</h4>
-      <form onSubmit={getSearch}>
-      <input className="search" type="date" value={search} onChange={updateSearch}></input>
+      <div className="headerDiv" className="flex flex-wrap justify-center flex-col">
+      <h1 className="text-6xl  my-auto mx-auto  md:mx-48 justify-center flex-wrap">astronomy photo of the day</h1>
+      <h4 className="smallHeader" className="text-6xl  my-auto mx-auto  md:mx-48 justify-center text-teal-400 ">created via nasa's open api</h4>
+      <form className="flex justify-center m-12" onSubmit={getSearch}>
+      <input className="search" className="flex justify-center text-gray-500 text-lg" type="date" value={search} onChange={updateSearch}></input>
       <br></br>
-      <Button className="search-button" type="submit">search</Button>
+      <Button className="search-button" className="flex justify-center p-2 px-4 ml-6" type="submit">search</Button>
       </form>
       </div>
       <Nasa title={nasaData.title} url={nasaData.url} date={nasaData.date} explanation={nasaData.explanation}/>
+      <br></br>
+        <br></br>
+        <br></br>
     </div>
   );
 }
