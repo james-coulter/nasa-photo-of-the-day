b0ae415535aa3b0b7804878e98882ec39d0a7aaf
Christian Coulter <christiancoulter@Christians-MacBook-Air-2.local>
2020-04-16 14:47:29 -0600

    created new branch to refactor css to tailwind

--- src/nasaContainer.js
+++ src/nasaContainer.js
@@ -4,15 +4,15 @@ import "./App.css"
 
 const Nasa = ({title, url, date, explanation}) => {
     return (
-        <div className="invisibleDiv">
-            <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'></link>
+        <div className="invisibleDiv" className="bg-gray-800 max-h-screen justify-center">
+            {/* <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'></link>
             <div id='stars'></div>
             <div id='stars2'></div>
-            <div id='stars3'></div>
-        <div className="largeWrapper">
-            <h4>{title}</h4>
+            <div id='stars3'></div> */}
+        <div className="largeWrapper" className="flex flex-wrap justify-center">
+            <h4 className="justify-center">{title}</h4>
             <h4>{date}</h4>
-            <div className="imageContainer">
+            <div className="imageContainer" className="m-0" >
             <img src={url} alt=""/>
             </div>
             <div className="explanationWrapper">
